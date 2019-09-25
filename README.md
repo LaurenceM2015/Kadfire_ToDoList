0494c90370f281cb37c93b4aaf7c60759572cbcb
LaurenceM2015 <laurencemalonga@hotmail.com>
2019-09-25 23:44:28 +0100

    Install postmain UI and create method.

--- README.md
+++ README.md
@@ -12,4 +12,19 @@ The only interaction with the API should be the initial call to get a list of to
 
 There aren’t any requirements for anything else, feel free implement the UI however you wish. Bootstrapping the app with create-react-app is preferred to save time but if you wish to use something else you can. You shouldn’t take more than an hour or so on this, if you find it taking longer, please write a brief summary of what you what have done if you had more time.
 
-Feel free to ask me any questions before you start.
+============================================
+ <ol>
+    <li>Create a React App by folowing the instruction from facebook github: https://github.com/facebook/create-react-app</li>
+    <li>Since I am gone use the fecth fetch() browser API to make the request, for the older browser might not support it, To increase the accessibilty, I gone need to install <strong>whatwg-fetch --save</strong> </li>
+    <li>Install reactstrap and peer dependencies via NPM</li>
+    <li>Create a constructor (props, and super) and used state</li>
+    <li>Delete all the unesery file, to start fresh.</li>
+    <li>Loaded that but once created delete method, failed to compile.</li>
+    <li>To fixe all the problem, I have reset the file from the first commit, and install Postman UI</li>
+    <li>For the add method I am gone use the models reactstrap components<li>
+    <li>Add a create method with a reactstrap form inpute</li>
+
+ </ol>
+
+
+
--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Table, Button, Modal, ModalHeader, ModalBody, ModalFooter,  FormGroup, Label, Input } from 'reactstrap';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // starting with an empty array.
+  state = {
+    // init the todo array
+    todos: [],
+    newTodoData: {
+      title: '',
+
+    },
+    newTodoModal: false
+  }
+
+ // Set the state
+  componentWillMount(){
+    axios.get('https://jsonplaceholder.typicode.com/todos').then((response) => {
+      this.setState({
+        todos: response.data
+      });
+      
+       
+    });
+  }
+  // Defining the method: NewTodoModal.bind(this) to update the state variable called new
+  toggleNewTodoModal() {
+    this.setState({
+      newTodoModal: ! this.state.new
+    });
+  }
+
+
+   render () {
+     // impliment state in the render method, to map each todoss
+     let todos = this.state.todos.map((todo) => {
+       // Return a single row
+        return (
+          <tr key={todo.userId}>
+            <td>{todo.userId}</td>
+            <td>{todo.id}</td>
+            <td>{todo.title}</td>
+            <td>{todo.completed}</td>
+            
+            <td>
+              <Button color="success" size="sm" className="mr-2">Edit</Button>
+              <Button color="danger" size="sm">Delete</Button>
+            </td>
+          </tr>
+        )
+     });
+    return (
+      <div className="App container">
+        <Button color="primary" onClick={this.toggleNewTodoModal.bind(this)}>Add A New To Do Task </Button>
+        
+        <Modal isOpen={this.state.newTodoModal} toggle={this.toggleNewTodoModal.bind(this)}>
+          
+
+          <ModalHeader toggle={this.toggleNewTodoModal.bind(this)}>Add a new Todo Task</ModalHeader>
+          
+          <ModalBody>
+            <FormGroup>
+              <Label for="title">Title</Label>
+              <Input type="text" id="title" value={this.state.newTodoData.title} onChange={(e) => {
+
+                // Getting new form data and then setting the title property to the new value of the input, and updating the state
+                let { newTodoData } = this.state;
+                 newTodoData.title = e.target.value;
+
+                 this.setState({ newTodoData });
+              }} />
+            </FormGroup>
+
+           
+
+              
+
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggleNewTodoModal.bind(this)}>Add To Do Task</Button>{' '}
+            <Button color="secondary" onClick={this.toggleNewTodoModal.bind(this)}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Id</th>
+              <th>Title</th>
+              <th>completed</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {todos}
+          </tbody>
+
+        </Table>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
